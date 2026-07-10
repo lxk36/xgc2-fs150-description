5800175c772349c332d9a3a95af188e5048ae22e
Lxk_buaa <867768510@qq.com>
2026-07-10 10:42:12 +0800

    ci: reuse trusted artifacts for APT releases

--- .xgc2/scripts/check_package_compliance.sh
+++ .xgc2/scripts/check_package_compliance.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 grep -q '^id: xgc2-fs150-description$' .xgc2/product.yml
-grep -q '^version: 0.1.0-1$' .xgc2/product.yml
+grep -q '^version: 0.1.0-2$' .xgc2/product.yml
 grep -q '<name>fs150_description</name>' package.xml
 grep -q 'ros-noetic-urdf' .xgc2/product.yml
 test -f meshes/iris.stl
